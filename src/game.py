7534ad588ab57b34afb33cf8ed9f42b45f60878c
Brian <brianzhou1102@gmail.com>
2025-07-03 08:33:31 -0400

    Ensure the first click always reveals a decent area

--- src/game.py
+++ src/game.py
@@ -1,6 +1,4 @@
-from Screen import Screen
-
-
+from screen import Screen
 import time
 import pygame
 import sys
@@ -25,7 +23,31 @@ KNIGHT = [
 NEIGHBORING = STANDARD  # edit this to change the game mode!
 MINE_PERCENT = 0.20
 assert MINE_PERCENT < 1, "MINE_PERCENT must be less than 1"
-GRID_SIZE = [10, 10]
+GRID_SIZE = [20, 20]
+CORNERS = [
+    # topleft
+    *[(0, 0), (0, 1), (1, 0), (1, 1)],
+    *[
+        (GRID_SIZE[0] - 1, 0),
+        (GRID_SIZE[0] - 1, 1),
+        (GRID_SIZE[0] - 2, 0),
+        (GRID_SIZE[0] - 2, 1),
+    ],  # topright
+    *[
+        (0, GRID_SIZE[1] - 1),
+        (1, GRID_SIZE[1] - 1),
+        (0, GRID_SIZE[1] - 2),
+        (1, GRID_SIZE[1] - 2),
+    ],  # bottomleft
+    *[
+        (GRID_SIZE[0] - 1, GRID_SIZE[1] - 1),
+        (GRID_SIZE[0] - 1, GRID_SIZE[1] - 2),
+        (GRID_SIZE[0] - 2, GRID_SIZE[1] - 1),
+        (GRID_SIZE[0] - 2, GRID_SIZE[1] - 2),
+    ],  # topright
+]
+CENTER_DISPERSAL_RADIUS = 1.5
+CORNER_DISPERSAL_RADIUS = 2.5
 NUM_MINES = int(MINE_PERCENT * GRID_SIZE[0] * GRID_SIZE[1])
 # endregion
 
@@ -70,15 +92,16 @@ PositionSet = set[Position]
 
 
 class Game(Screen):
-    won = False
-
     def __init__(self, screen: pygame.Surface) -> None:
         super().__init__(screen)
-        self.mine_field, self.mine_positions = self.mine_field_set_up()
+        self.mine_field: Grid = []
+        self.mine_positions: PositionSet = set()
+        self.won = False
+        self.first_click = True
         self.revealed: PositionSet = set()
         self.flagged: PositionSet = set()
         self.starting_time = pygame.time.get_ticks()
-    
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -120,6 +143,9 @@ class Game(Screen):
         if event.button == pygame.BUTTON_LEFT:
             if pos in self.revealed or pos in self.flagged:
                 return
+            if self.first_click:
+                self.mine_field_set_up(pos)
+                self.first_click = False
             self.reveal_zero_tiles(pos)
         elif event.button == pygame.BUTTON_MIDDLE:
             if pos not in self.revealed:
@@ -132,7 +158,6 @@ class Game(Screen):
                 self.flagged.remove(pos)
             elif len(self.flagged) != NUM_MINES:
                 self.flagged.add(pos)
-
 
     def show_mines(self):
         horizontal = self.screen.get_width() / GRID_SIZE[0]
@@ -195,13 +220,14 @@ class Game(Screen):
             for unrevealed in unrevealed_neighbors:
                 self.flagged.add(unrevealed)
 
-
     def reveal_zero_tiles(self, pos: Position):
         if self.val_at_pos(pos) != 0:
             self.revealed |= {pos}
             return
         just_revealed = {pos} | set(neighbor for neighbor in self.neighbors(pos))
-        to_check = set(neighbor for neighbor in self.neighbors(pos) if self.val_at_pos(neighbor) == 0)
+        to_check = set(
+            neighbor for neighbor in self.neighbors(pos) if self.val_at_pos(neighbor) == 0
+        )
         while len(to_check) > 0:
             tile = to_check.pop()
             just_revealed.add(tile)
@@ -210,12 +236,13 @@ class Game(Screen):
                     continue
                 if self.val_at_pos(neighbor) == 0:
                     to_check.add(neighbor)
-                else: 
-                    assert self.val_at_pos(neighbor) != MINE, "Revealed a mine through reveal_zero_tile"
+                else:
+                    assert (
+                        self.val_at_pos(neighbor) != MINE
+                    ), "Revealed a mine through reveal_zero_tile"
                     just_revealed.add(neighbor)
         self.revealed |= just_revealed
         assert self.flagged & just_revealed == set(), "No revealed mines should have been flagged"
-
 
     def draw_mine_field(self):
         horizontal = self.screen.get_width() / GRID_SIZE[0]
@@ -224,7 +251,9 @@ class Game(Screen):
 
         starting_color = (74, 117, 44)
         self.screen.fill(starting_color)
-        pygame.draw.rect(self.screen, starting_color, pygame.rect.Rect(150, 0, block_size, block_size))
+        pygame.draw.rect(
+            self.screen, starting_color, pygame.rect.Rect(150, 0, block_size, block_size)
+        )
 
         mouse_pos = pygame.mouse.get_pos()
         converted_mouse_pos = (
@@ -248,7 +277,9 @@ class Game(Screen):
                     x * block_size, y * block_size + HEADER_SIZE, block_size, block_size
                 )
                 if (x, y) in self.revealed:
-                    pygame.draw.rect(self.screen, DARK_EMPTY if (x + y) % 2 == 1 else LIGHT_EMPTY, rect)
+                    pygame.draw.rect(
+                        self.screen, DARK_EMPTY if (x + y) % 2 == 1 else LIGHT_EMPTY, rect
+                    )
                     if self.mine_field[x][y] == MINE:
                         continue
                     pos = [(x + 0.5) * block_size, (y + 0.5) * block_size + HEADER_SIZE]
@@ -270,7 +301,6 @@ class Game(Screen):
 
         pygame.display.update()
 
-
     def draw_tile_nums(self, pos, num, size):
         if num == 0:
             return
@@ -278,31 +308,44 @@ class Game(Screen):
         text = font.render(str(num), True, COLOR_PALETTE[num])
         self.screen.blit(text, text.get_rect(center=pos))
 
-
     def make_text(self, pos, text, size, color):
         font = pygame.font.SysFont(pygame.font.get_default_font(), int(size))
         text = font.render(str(text), True, color)
         self.screen.blit(text, text.get_rect(midleft=pos))
 
+    def mine_field_set_up(self, mouse_pos: Position):
+        possible_locations = list(
+            (x, y) for x, y in product(range(GRID_SIZE[0]), range(GRID_SIZE[1]))
+        )
 
-    def mine_field_set_up(self) -> tuple[Grid, PositionSet]:
-        possible_locations = list(product(range(GRID_SIZE[0]), range(GRID_SIZE[1])))
-        mine_locations = {
+        mx, my = mouse_pos
+        radius = CORNER_DISPERSAL_RADIUS if mouse_pos in CORNERS else CENTER_DISPERSAL_RADIUS
+        i = 0
+        count = 0
+        while i < len(possible_locations):
+            x, y = possible_locations[i]
+            if abs(mx - x) ** 2 + abs(my - y) ** 2 <= radius**2:
+                possible_locations.pop(i)
+                count += 1
+                continue
+            i += 1
+        print(f"Count: {count}")
+
+        self.mine_positions = {
             possible_locations.pop(randrange(len(possible_locations))) for _ in range(NUM_MINES)
         }
 
         self.mine_field: Grid = [[0] * GRID_SIZE[1] for _ in range(GRID_SIZE[0])]
 
-        for mine_location in mine_locations:
+        for mine_location in self.mine_positions:
             self.mine_field[mine_location[0]][mine_location[1]] = MINE
             for neighbor in self.neighbors(mine_location):
-                if neighbor in mine_locations:
+                if neighbor in self.mine_positions:
                     continue
-                assert self.mine_field[neighbor[0]][neighbor[1]] != "M", "Mine not marked in mine_locations"
+                assert (
+                    self.mine_field[neighbor[0]][neighbor[1]] != "M"
+                ), "Mine not marked in mine_locations"
                 self.mine_field[neighbor[0]][neighbor[1]] += 1  # type: ignore
-
-        return self.mine_field, mine_locations
-
 
     def neighbors(self, pos: Position):
         x, y = pos
@@ -311,13 +354,11 @@ class Game(Screen):
             if 0 <= nx < GRID_SIZE[0] and 0 <= ny < GRID_SIZE[1]:
                 yield (nx, ny)
 
-
     def val_at_pos(self, pos):
         return self.mine_field[int(pos[0])][int(pos[1])]
-    
-    
+
     def get_starting_time(self):
         return self.starting_time
-    
+
     def did_win(self):
         return self.won
